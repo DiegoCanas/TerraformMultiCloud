1e487fb2549098896b4e8e87c7e77511808b9233
Diego Cañas <diegocanas@MacBook-Pro-de-Diego.local>
2023-10-23 17:50:54 +0200

    23o3

---
+++ AWS/modules/microservice-instance/ms-output.tf
@@ -0,0 +1,13 @@
+resource "random_id" "app_name_suffix" {
+  byte_length = 4
+}
+
+resource "aws_instance" "apps" {
+
+  for_each = toset(var.zones )  #Permite crear una instancia de EC2 para cada zona de disponibilidad definida
+  availability_zone = each.key
+  ami = var.image
+  instance_type = var.instance_type
+  tags = {  
+    Name = "apps-${random_id.app_name_suffix.hex}"
+  }
---
+++ AWS/modules/microservice-instance/ms-vars.tf
@@ -0,0 +1,4 @@
+variable appserver_count {}
+variable instance_type {}
+variable image {}
+variable zones {}
